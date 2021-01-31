246c77e190df4c263aa67cb4bfb1c289bae66313
Cao Minh <trancaominh1997@gmail.com>
2021-01-31 21:08:32 +0700

    code update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BannerHeaderComponent } from './banner-header/banner-header.component';
 import { TopHeaderComponent } from './top-header/top-header.component';
+import { BuyNowComponent } from './buy-now/buy-now.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BannerHeaderComponent,
-    TopHeaderComponent
+    TopHeaderComponent,
+    BuyNowComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/buy-now/buy-now.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-buy-now',
+  templateUrl: './buy-now.component.html',
+  styleUrls: ['./buy-now.component.css']
+})
+export class BuyNowComponent implements OnInit {
+
+  // cons buyNow: String = [
+  //   'Phim',
+  //   'Rạp',
+  //   'Ngày Xem',
+  //   'Suất Chiếu',
+  //   'Mua Vé Ngay'
+  // ];
+
+  constructor() { }
+
+
+  ngOnInit(): void {
+  }
+
+}
